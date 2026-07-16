9868e5052f077a7cf3a19cdc560d59057e5130bc
balbir singh <balbirsingh12@gmail.com>
2026-07-16 21:56:19 +0530

    Day 2: Learned Python variables and input

---
+++ variables.py
@@ -0,0 +1,17 @@
+
+# Variables
+
+name = "Balbir"
+age = 22
+cgpa = 8.1
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("CGPA:", cgpa)
+print("Student:", is_student)
+
+# Input
+
+city = input("Enter your city: ")
+print("City:", city)
